8a07ffa9285297b5fe41eb19075ee46a8e589804
thekayshawn <kashanahmad.me@gmail.com>
2024-12-31 15:20:37 +0500

    chore: remove unwanted files

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,2 @@
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
+// Whatever you export from here will be available to the consumers of your library
+export * from "./lib/sum";
